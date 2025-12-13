d31a2367aece2b64e5d6bbf2752ba6bf5e8c513b
froonn <137333195+froonn@users.noreply.github.com>
2025-12-13 04:42:35 +0300

    fix doubled added 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider> {/* <-- Обертываем приложение в CartProvider */}
@@ -16,5 +16,5 @@ root.render(
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
